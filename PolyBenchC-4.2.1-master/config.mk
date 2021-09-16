7238180999957980f9ebb3cd42233261c50a8832
William S. Moses <gh@wsmoses.com>
2021-09-16 12:42:48 -0400

    Minor fixup

--- PolyBenchC-4.2.1-master/config.mk
+++ PolyBenchC-4.2.1-master/config.mk
@@ -1,16 +1,21 @@
 
-CC=$${HOME}/Polygeist/build/mlir-clang/mlir-clang
+CC=$${HOME}/git/Polygeist/build/mlir-clang/mlir-clang
 CFLAGS=-L ~/llvm-project/build/lib \
-       -resource-dir=$${HOME}/llvm-project/build/lib/clang/14.0.0/ \
+       -resource-dir=$${HOME}/git/Polygeist/mlir-build/lib/clang/14.0.0/ \
        -DLARGE_DATASET \
+	   --function=* \
        -lm
 
-CUCC=$${HOME}/Polygeist/build/mlir-clang/mlir-clang
+CUCC=$${HOME}/git/Polygeist/build/mlir-clang/mlir-clang
 CUCFLAGS=-L ~/llvm-project/build/lib \
-         -resource-dir=$${HOME}/llvm-project/build/lib/clang/14.0.0/ \
+         -resource-dir=$${HOME}/git/Polygeist/mlir-build/lib/clang/14.0.0/ \
          --cuda-gpu-arch=sm_60 \
          --cuda-lower \
-         --cpuify="continuation" \
+         --cpuify="distribute" \
          -DLARGE_DATASET \
+	     --function=* \
+		 -L /usr/local/cuda-11.2/lib64 \
+		 -L ${HOME}/git/Polygeist/mlir-build/lib \
          -lcudart_static -ldl -lrt -lpthread -lm
 
+         #--cpuify="continuation" \
